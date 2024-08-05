3d40f33e64d07fc8f3c0f0e660183a499097f451
Praneethreddy264 <balapraneethreddy2004@gmail.com>
2024-08-05 08:28:45 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -3,9 +3,10 @@ print('kastamee idi kaasta')
 print("akkada ikkada")
 print("ekkada")
 print("jana gana mana nenu evari mata vina")
+
 print("ahrgagfiaef")
-=======
+
 print("bro whatsupp")
 print("fasjhbg")
->>>>>>> Stashed changes
 print("l")
+print("helooooooo")
