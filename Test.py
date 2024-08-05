4c3fff92373bb79cf0dc4308edf80ea2e1f9221a
Praneethreddy264 <balapraneethreddy2004@gmail.com>
2024-08-05 08:38:01 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -3,9 +3,7 @@ print('kastamee idi kaasta')
 print("akkada ikkada")
 print("ekkada")
 print("jana gana mana nenu evari mata vina")
-
 print("ahrgagfiaef")
-
 print("bro whatsupp")
 print("fasjhbg")
 print("l")
